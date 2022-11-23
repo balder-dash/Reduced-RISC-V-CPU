cb60afa1c226824547793b05c242baf100d2d2bc
balder-dash <105637297+balder-dash@users.noreply.github.com>
2022-11-23 14:39:07 +0000

    Update Mux_1.sv

--- Erenkai Zhu/Mux_1.sv
+++ Erenkai Zhu/Mux_1.sv
@@ -1,7 +1,8 @@
 module mux1(input logic [3:0] branch_PC,
-            input logic     s,
+            input logic     PCsrc,
+            input logic inc_PC,
             output logic [3:0] next_PC);
-    assign y= s? branch_PC:inc_PC;
+    assign next_PC = PCsrc? branch_PC:inc_PC;
 endmodule
 
 
