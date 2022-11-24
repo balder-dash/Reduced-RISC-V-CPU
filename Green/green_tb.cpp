70f4cbba2a30ed13a47f25ba9a8924be4dbbc7af
balder-dash <ckcliff88@gmail.com>
2022-11-24 11:26:37 +0000

    First commit

---
+++ Green/green_tb.cpp
@@ -0,0 +1,36 @@
+#include <verilated.h>
+#include <verilated_vcd.h>
+#include <Vtop.h>
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated:: commandArgs(argc, argv);
+    
+    Vcounter* top = new Vcounter;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp,99);
+    tfp->open ("counter.vcd");
+
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    for (i = 0; i < 300; i++){
+
+        //dump var into VCD file and toggle clk
+        for(clk = 0; clk < 2; clk++){
+            tfp->dump(2*i+clk); //unit is in ps
+            top->clk = !top->clk;
+            top->eval ();
+        }
+        top->rst =(i < 2 | i == 15);
+        top->en = (i>4);
+        if (Verilated::gotFinish()) exit(0);
+    }
+    tfp->close();
+    exit(0);
+}
